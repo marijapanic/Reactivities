d67cd31bde549223275fb9126cd0b5ee7e4a8b94
marijapanic <mpanic432@gmail.com>
2023-03-01 22:47:24 +0100

    Add initial commit regarding user identity

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LogInDto logInDto)
+        {
+            var user = await _userManager.FindByEmailAsync(logInDto.Email);
+            if (user == null) return Unauthorized();
+
+            var result = await _userManager.CheckPasswordAsync(user, logInDto.Password);
+
+            if (result)
+            {
+                return new UserDto
+                {
+                    Displayname = user.DisplayName,
+                    Image = null,
+                    Token = "This will be a token",
+                    Username = user.UserName
+                };
+            }
+
+            return Unauthorized();
+        }
+    }
+}
